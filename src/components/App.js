4817bf864ea004d927f399532df4ef7d6f9d5475
SandraND <lainfaith2@gmail.com>
2018-10-11 11:47:29 +0200

    add search bar in app

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,53 @@
 import React, { Component } from 'react';
 import Header from './Header';
-import Workers from './Worker/Workers';
+import WorkerList from './Worker/WorkerList';
+import SearchBar from './SearchBar/SearchBar';
 
-export default class App extends Component{
+class App extends Component {
 
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
     this.state= {
-      title: `Ommpa Loompa's Crew`
+      title: `Ommpa Loompa's Crew`,
+      workers: [],
+      term: ''
     };
+  }
+
+  componentDidMount() {
+    if(this.state.term !== '') {
+      this.fetchWorkers(this.state.term)
+    } else {
+      this.fetchWorkers('coding');
+    }
+  }
+
+  fetchWorkers = (term) => {
+    fetch('https://2q2woep105.execute-api.eu-west-1.amazonaws.com/napptilus/oompa-loompas')
+    .then(resp => {return resp.json()})
+    .then(data => {
+      this.setState({
+        workers: data.results
+      })
+    })
+  }
+
+  changeSearchTermState = (event) => {
+    this.setState({
+      term: event.target.value
+    }, () => {this.state.term === '' ? this.fetchWorkers('coding') : this.fetchWorkers(this.state.term)})
   }
 
   render() {
     return (
       <div>
-        <Header title={this.state.title}>
-          <div className='mt-5'>
-            <Workers />
-          </div>
-        </Header>
+        <Header title={this.state.title} />
+        <SearchBar changeSearchTermState={this.changeSearchTermState} value={this.state.term} />
+        <WorkerList workers={this.state.workers} />
       </div>
     );
   }
 }
+
+export default App;
 
